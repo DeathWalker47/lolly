442810a5ca18de6604f63928d3325533c11e114e
DeathWalker47 <fizer_00@mail.ru>
2021-08-27 21:32:51 +0300

    добавил хэдер, топ сексию, подключил слайдер свайпер

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,25 @@
+
+ new Swiper('.swiper',{
+  loop:true,
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  speed: 2000,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+ });
+
 $(function(){
   
 });

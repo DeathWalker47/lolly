51936bf21d560cebe07b0e39bade67cd85904033
DeathWalker47 <fizer_00@mail.ru>
2021-09-01 20:44:31 +0300

    добавил страницу корзины

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,11 @@
 
 $(function () {
+
+  $('.cart-item__deleted').on('click', function(e){
+    e.preventDefault();
+
+    $($(this).attr('href')).addClass('cart-item--delete');
+  });
  
   $(".star").rateYo({
     starWidth: "14px",
@@ -8,7 +14,6 @@ $(function () {
     halfStar: true,
     spacing: "3px",
     readOnly: true,
-
     //"starSvg": '<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 0.700012L4.9 4.90001L0.699997 5.42501L3.591 8.65901L2.8 13.3L7 11.2L11.2 13.3L10.416 8.65901L13.3 5.42501L9.1 4.90001L7 0.700012ZM7 2.26801L8.638 5.55101L11.893 5.95701L9.667 8.44901L10.276 12.054L7 10.416L3.724 12.054L4.333 8.44901L2.107 5.95701L5.362 5.55101L7 2.26801Z" fill="#DF949D"/><path xmlns="http://www.w3.org/2000/svg" d="M6.99999 0.700012L9.09999 4.90001L13.3 5.42501L10.416 8.65901L11.2 13.3L6.99999 11.2L2.79999 13.3L3.59099 8.65901L0.69999 5.42501L4.89999 4.90001L6.99999 0.700012Z" /></svg>'
     "starSvg": '<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 0.700012L4.9 4.90001L0.699997 5.42501L3.591 8.65901L2.8 13.3L7 11.2L11.2 13.3L10.416 8.65901L13.3 5.42501L9.1 4.90001L7 0.700012ZM7 2.26801L8.638 5.55101L11.893 5.95701L9.667 8.44901L10.276 12.054L7 10.416L3.724 12.054L4.333 8.44901L2.107 5.95701L5.362 5.55101L7 2.26801Z" fill="#DF949D"/><path xmlns="http://www.w3.org/2000/svg" d="M6.99999 0.700012L9.09999 4.90001L13.3 5.42501L10.416 8.65901L11.2 13.3L6.99999 11.2L2.79999 13.3L3.59099 8.65901L0.69999 5.42501L4.89999 4.90001L6.99999 0.700012Z"/></svg>'
   });
@@ -31,8 +36,8 @@ $(function () {
 
     $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
     $($(this).attr('href')).addClass('product-tabs__content-item--active');
-  });
 
+  });
 
 
   $('<div class="quantity-nav"><div class="quantity-button quantity-up">+</div><div class="quantity-button quantity-down">-</div></div>').insertAfter('.quantity input');
